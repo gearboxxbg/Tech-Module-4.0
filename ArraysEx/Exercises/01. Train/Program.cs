078f9eb1f5d0106353a4f4ebdcd69a689ca99c6d
Borislav Kunchev <gearboxxbg@gmail.com>
2019-02-25 12:51:00 +0200

    Added "Train"

---
+++ ArraysEx/Exercises/01. Train/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		int wagons = int.Parse(Console.ReadLine());
+		int[] passengers = new int[wagons];
+		int sum = 0;
+
+		for (int i = 0; i < wagons; i++)
+		{
+			int number = int.Parse(Console.ReadLine());
+			passengers[i] = number;
+			sum += number;
+		}
+
+		for (int i = 0; i < wagons; i++)
+		{
+			Console.Write(passengers[i] + " ");
+		}
+
+		Console.WriteLine();
+		Console.WriteLine(sum);
+	}
+}
+}
