ca69cc48c059e050511f204d5e5234d75fddfce9
Borislav Kunchev <gearboxxbg@gmail.com>
2019-02-26 01:08:49 +0200

    Added "Array rotation" / fixed

---
+++ ArraysEx/Exercises/Array Rotation/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		int[] numbers = Console.ReadLine().Split(" ")
+		.Select(int.Parse)
+		.ToArray();
+		int rotations = int.Parse(Console.ReadLine());
+
+		if (rotations > numbers.Length)
+		{
+			rotations -= numbers.Length;
+		}
+		else if (rotations == numbers.Length)
+		{
+			for (int i = 0; i < numbers.Length; i++)
+			{
+				Console.Write(numbers[i] + " ");
+			}
+			return;
+		}
+
+		int index = 0;
+		int[] resultArray = new int[numbers.Length];
+
+		for (int i = rotations; i< numbers.Length; i++)
+		{
+			resultArray[index] = numbers[i];
+			index++;
+		}
+
+		for (int i = 0; i < rotations; i++)
+		{
+			resultArray[index] = numbers[i];
+			index++;
+		}
+
+		for (int i = 0; i < resultArray.Length; i++)
+		{
+			Console.Write(resultArray[i] + " ");
+		}
+
+		Console.WriteLine();
+	}
+}
