5134ae1163a3479e99478a32e9c14d4f972a6ab9
Borislav Kunchev <gearboxxbg@gmail.com>
2019-02-25 23:52:05 +0200

    Added "Common elements" finished

---
+++ ArraysEx/Exercises/02.Common Elements/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string[] firstArray = Console.ReadLine().Split(" ");
+		string[] secondArray = Console.ReadLine().Split(" ");
+
+		for (int i = 0; i < firstArray.Length; i++)
+		{
+			for (int j = 0; j < secondArray.Length; j++)
+			{
+				if (firstArray[i] == secondArray[j])
+				{
+					Console.Write(firstArray[i] + " ");
+				}
+			}
+		}
+	}
+}
