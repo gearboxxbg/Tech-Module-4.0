a6134255d2e8253f5d4b7f77c30c271f8c5a29d6
Borislav Kunchev <gearboxxbg@gmail.com>
2019-02-26 00:09:10 +0200

    Added "Zig-Zag arrays" finished

---
+++ ArraysEx/Exercises/Zig-Zag Arrays/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		int number = int.Parse(Console.ReadLine());
+		int[] firstArray = new int[number];
+		int[] secondArray = new int[number];
+		int count = 0;
+
+		for (int i = 0; i < number; i++)
+		{
+			int[] values = Console.ReadLine().Split(" ")
+				.Select(int.Parse)
+				.ToArray();
+
+			if (i % 2 == 0)
+			{
+				firstArray[count] = values[0];
+				secondArray[count] = values[1];
+				count++;
+			}
+			else if (i % 2 != 0)
+			{
+				firstArray[count] = values[1];
+				secondArray[count] = values[0];
+				count++;
+			}
+		}
+
+		Console.WriteLine(string.Join(" ", firstArray));
+		Console.WriteLine(string.Join(" ", secondArray));
+	}
+}
+
