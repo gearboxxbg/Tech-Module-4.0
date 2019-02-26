05c2a6ead598a24eca2061be1d632ca21dec7360
Borislav Kunchev <gearboxxbg@gmail.com>
2019-02-26 08:45:13 +0200

    Added "Max sequence of equal elements" to the solution

---
+++ ArraysEx/Exercises/Max Sequence of Equal Elements/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+		int longestSeqLength = 1;
+		int longestSeqStart = 0;
+		int currentSeqLength = 1;
+		int currentSeqStart = 0;
+
+		for (int i = 1; i < numbers.Length; i++)
+		{
+			if (numbers[i] == numbers[i - 1])
+			{
+				currentSeqLength++;
+
+				if (currentSeqLength > longestSeqLength)
+				{
+					longestSeqLength = currentSeqLength;
+					longestSeqStart = currentSeqStart;
+				}
+			}
+			else
+			{
+				currentSeqLength = 1;
+				currentSeqStart = i;
+			}
+		}
+
+		for (int i = longestSeqStart; i < longestSeqStart + longestSeqLength; i++)
+		{
+			Console.Write(numbers[i] + " ");
+		}
+	}
+}
