400fc859188151e33bb483521e0ffedcfe5d1e6c
Borislav Kunchev <gearboxxbg@gmail.com>
2019-02-26 08:04:20 +0200

    Added "Top integer" to the solution

---
+++ ArraysEx/Exercises/Top Integers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		int[] numbers = Console.ReadLine().Split(" ")
+			.Select(int.Parse)
+			.ToArray();
+		int index = 0;
+
+		foreach (int number in numbers)
+		{
+			bool isTopInt = true;
+
+			for (int i = index + 1; i < numbers.Length; i++)
+			{
+				if (number <= numbers[i])
+				{
+					isTopInt = false;
+				}
+			}
+
+			if (isTopInt)
+			{
+				Console.Write(number + " ");
+			}
+
+			index++;
+		}
+	}
+}
+
