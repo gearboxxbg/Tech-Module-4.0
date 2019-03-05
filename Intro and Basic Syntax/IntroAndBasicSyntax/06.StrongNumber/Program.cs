890934edabfef3a92fdd1b6c480c9ee992279531
Borislav Kunchev <gearboxxbg@gmail.com>
2019-03-05 18:22:44 +0200

    Added  "Strong Number" solution and readme.md

---
+++ Intro and Basic Syntax/IntroAndBasicSyntax/06.StrongNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int inputNum = int.Parse(Console.ReadLine());
+			long checkNum = inputNum;
+			long factorielSum = 0;
+
+			while (checkNum != 0)
+			{
+				long result = checkNum % 10;
+				checkNum /= 10;
+
+				long factorial = 1;
+				for (int i = 1; i <= result; i++)
+				{
+					factorial *= i;
+				}
+				factorielSum += factorial;
+			}
+
+			if (factorielSum == inputNum)
+			{
+				Console.WriteLine("yes");
+			}
+			else
+			{
+				Console.WriteLine("no");
+			}
+		}
+	}
