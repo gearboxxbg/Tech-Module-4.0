6f559bfdde456365755d9cecb7b20e3feb4cb132
Borislav Kunchev <gearboxxbg@gmail.com>
2019-03-14 01:53:15 +0200

    Added "Cooking Factory"solution and readme

---
+++ ExamPrep/ExamPrep/ExamPrep/03.CookingFactorty/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		int[] bestBatch = new int[] { int.MinValue };
+		int bestQuality = int.MinValue;
+		string input = string.Empty;
+
+		while ((input = Console.ReadLine()) != "Bake It!")
+		{
+			int[] currentBatch = input.Split('#').Select(int.Parse).ToArray();
+			int currentQuality = currentBatch.Sum();
+			bool foundBetterBatch = false;
+
+			if (bestQuality < currentQuality)
+			{
+				foundBetterBatch = true;
+			}
+			else if (bestQuality == currentQuality)
+			{
+				if (bestBatch.Average() < currentBatch.Average())
+				{
+					foundBetterBatch = true;
+				}
+				else if (bestBatch.Average() == currentBatch.Average() &&
+						 bestBatch.Length > currentBatch.Length)
+				{
+					foundBetterBatch = true;
+				}
+			}
+
+			if (foundBetterBatch)
+			{
+				bestBatch = (int[])currentBatch.Clone();
+				bestQuality = bestBatch.Sum();
+			}
+		}
+
+		Console.WriteLine($"Best Batch quality: {bestQuality}\n{string.Join(" ", bestBatch)}");
+	}
+}
---
+++ ExamPrep/ExamPrep/ExamPrep/03.CookingFactorty/READMEc.md
@@ -0,0 +1,12 @@
+# Problem 3. Cooking Factory
+The George`s Cooking factory got another order. But this time you are tasked to bake 
+the best Bread for a special party.
+Until you receive a command "Bake it!" you will be receiving strings, the batches of bread. Each string is an array of numbers, split by "#". Each element is a bread and the number represent its quality.
+You should select the batch with the highest total quality of bread.
+If there are several batches with same total quality select the batch with the greater average quality.
+If there are several batches with same total quality and average quality, take the one with the fewest elements (length).
+
+Input / Constraints
+•	Until you receive a command "Bake it!" you will be receiving strings, the batches of bread. Each string is an array of numbers, split by "#". Each element is a bread and the number represent its quality.
+•	Each batch will have from 1 to 10 elements.
+•	Bread quality is an integer in the range [-100, 100].
