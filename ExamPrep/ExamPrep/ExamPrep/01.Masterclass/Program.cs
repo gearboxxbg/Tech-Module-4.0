abbd9057ae57162046fe1f21ad3b1932461598e7
Borislav Kunchev <gearboxxbg@gmail.com>
2019-03-14 01:45:22 +0200

    Added "Cooking Masterclass"solution and readme.md

---
+++ ExamPrep/ExamPrep/ExamPrep/01.Masterclass/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		float budget = float.Parse(Console.ReadLine());
+		int students = int.Parse(Console.ReadLine());
+		float priceOfFlour = float.Parse(Console.ReadLine());
+		float priceOfEggs = float.Parse(Console.ReadLine());
+		float priceOfApron = float.Parse(Console.ReadLine());
+		float percentOfApron = ((float)students) + (float)students * 0.2F;
+		float apronPrice = priceOfApron * (float)(Math.Ceiling(percentOfApron));
+		int freePackages = students / 5;
+		float eggsPrice = (priceOfEggs * 10.0F) * (float)students;
+		float flourPrice = priceOfFlour * ((float)students - freePackages);
+		float totalMoneyForItems = apronPrice + eggsPrice + flourPrice;
+		float needMoneyForItems = totalMoneyForItems - budget;
+
+		if (budget >= totalMoneyForItems)
+		{
+			Console.WriteLine($"Items purchased for {totalMoneyForItems:F2}$.");
+		}
+		else
+		{
+			Console.WriteLine($"{needMoneyForItems:F2}$ more needed.");
+		}
+	}
+}
